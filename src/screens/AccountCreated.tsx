90d7ccbed71e49b6bdcd08a931c64f4ff6f0c26c
J0ão Pedro <jpmmatias2018@outlook.com>
2021-03-02 11:35:37 -0300

    Atualizou screen de conta criada

--- src/screens/AccountCreated.tsx
+++ src/screens/AccountCreated.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import { AuthNavProps } from '../types';
+// @ts-ignore
+import like from '../assets/images/like.png';
+import { Center } from '../components/Center';
 const { width, height } = Dimensions.get('window');
 
 const AccountCreated = ({
@@ -13,56 +16,24 @@ const AccountCreated = ({
 		}, 5000);
 	}, []);
 	return (
-		<View style={styles.container}>
-			<Image
-				width={200}
-				height={200}
-				source={require('../assets/images/like.png')}
-			/>
-			<Text>Conta criada com sucesso!</Text>
-		</View>
+		<Center>
+			<Image style={styles.like} source={like} />
+			<Text style={styles.text}>Conta criada com sucesso!</Text>
+		</Center>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#8C52E5',
-		alignItems: 'center',
+	text: {
+		fontFamily: 'Roboto-Medium',
+		color: '#fff',
+		fontSize: 20,
 	},
-	form: {
-		marginBottom: 37,
-	},
-	logo: {
-		height: 55,
-		width: 249,
-		marginTop: 40,
-		marginBottom: 60,
-	},
-	cardBody: {
-		width: (width * 70) / 100,
-		height: (height * 66) / 100,
-	},
-	title: {
-		marginTop: 5,
-		fontSize: 21,
-		textAlign: 'center',
-		fontWeight: '500',
-		color: '#1d1d1d',
-		lineHeight: 24.61,
-		marginBottom: 60,
-	},
-	link: {
-		fontWeight: '500',
-		fontSize: 13,
-		marginBottom: 20,
-		color: '#8C52E5',
-		textAlign: 'center',
-	},
-	input: {
-		padding: 5,
-		borderBottomWidth: 1,
-		borderBottomColor: '#878686',
+
+	like: {
+		height: 162,
+		width: 162,
+		marginBottom: 30,
 	},
 });
 
