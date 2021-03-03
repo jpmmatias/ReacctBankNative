22f715a287ea92d12057e656360eb13d4b95b1bf
J0ão Pedro <jpmmatias2018@outlook.com>
2021-03-03 09:20:40 -0300

    Realizou ajustes

--- src/screens/Planos.tsx
+++ src/screens/Planos.tsx
@@ -4,20 +4,25 @@ import { Center } from '../components/Center';
 import Card from '../components/Card';
 import api from '../services/api';
 import Toast from 'react-native-toast-message';
-import {  AppTabNavProps, IListData, IPlanoconta, IUser } from '../types';
+import { AppTabNavProps, IListData, IPlanoconta, IUser } from '../types';
 import { FlatList, TextInput } from 'react-native-gesture-handler';
-import {Picker} from '@react-native-community/picker';
-import { useStore } from 'react-redux';
+import { Picker } from '@react-native-community/picker';
+import { useDispatch, useStore } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { putPlanoConta, savePlanosConta } from '../store/modules/user/action';
 
 const Planos = ({ navigation, route }: AppTabNavProps<'Planos'>) => {
-	const store = useStore()
-	const user:IUser = store.getState().user.user
+	const store = useStore();
+	const user: IUser = store.getState().user.user;
+	const globalPlanosConta: IPlanoconta[] = store.getState().user.planosConta;
+	const dispatch = useDispatch();
 
-	const [listData, setListData] = useState<IListData[]>([])
-	const [planosConta, setPlanosConta] = useState<IPlanoconta[]>([])
-	const [descricao, setDescricao] = useState('')
-	const [tipoMovimento, setTipoMovimento] = useState('')
+	console.log(globalPlanosConta);
+
+	const [listData, setListData] = useState<IListData[]>([]);
+	const [planosConta, setPlanosConta] = useState<IPlanoconta[]>([]);
+	const [descricao, setDescricao] = useState('');
+	const [tipoMovimento, setTipoMovimento] = useState('');
 
 	const [novoPlano, setNovoPlano] = useState<IPlanoconta>({
 		descricao: '',
@@ -31,106 +36,115 @@ const Planos = ({ navigation, route }: AppTabNavProps<'Planos'>) => {
 		method(value);
 		setNovoPlano({ ...novoPlano, [campo]: value });
 	}
-  
-	async function adicionarPlanoContas(){
-		let id = planosConta[planosConta.length - 1].id + 1;
-		let plano = { ...novoPlano, id };
-		api.post(`lancamentos/planos-conta?login=${plano.login}`,plano,  {
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: await AsyncStorage.getItem('@tokenApp'),
-			},
-		})
-		.then(res =>{
-			Toast.show({
-				type: 'success',
-				position: 'top',
-				text1: "sucesso",	 			
-			});
-		}).catch(err => {
-			Toast.show({
-				type: 'error',
-				position: 'top',
-				text1: err.message,	 			
-			});
-		})
-	
-	}
 
-useEffect(()=>{
-		async function load(){
-			console.log(await AsyncStorage.getItem('@tokenApp'))
-			api.get(`/lancamentos/planos-conta?login=${user.login}`,{
+	async function adicionarPlanoContas() {
+		let id = globalPlanosConta[globalPlanosConta.length - 1].id + 1;
+		let plano = { ...novoPlano, id };
+		api
+			.post(`lancamentos/planos-conta?login=${plano.login}`, plano, {
 				headers: {
 					'Content-Type': 'application/json',
 					Authorization: await AsyncStorage.getItem('@tokenApp'),
 				},
 			})
-			.then(res =>{
+			.then((res) => {
 				Toast.show({
 					type: 'success',
 					position: 'top',
-					text1: "sucesso",	 			
+					text1: 'sucesso',
 				});
-				
-				let planosContaAux:IPlanoconta[] = res.data
-				let result:IListData[] = []
-				for(let i=0; i<planosContaAux.length; i++){
-					result.push({key:planosContaAux[i].descricao+" ("+planosContaAux[i].tipoMovimento+")"})
-				}
-	
-				setListData(result)
-				setPlanosConta(res.data)
-							
-			}).catch(err =>{
-				Toast.show({
-					type: 'error',
-					position: 'top',
-					text1: err.message,	 			
-				});
+				dispatch(putPlanoConta(plano));
 			})
+			.catch((err) => {});
+	}
+
+	useEffect(() => {
+		async function load() {
+			console.log(await AsyncStorage.getItem('@tokenApp'));
+			api
+				.get(`/lancamentos/planos-conta?login=${user.login}`, {
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: await AsyncStorage.getItem('@tokenApp'),
+					},
+				})
+				.then((res) => {
+					Toast.show({
+						type: 'success',
+						position: 'top',
+						text1: 'sucesso',
+					});
+
+					let planosContaAux: IPlanoconta[] = res.data;
+					dispatch(savePlanosConta(planosContaAux));
+					let result: IListData[] = [];
+					for (let i = 0; i < planosContaAux.length; i++) {
+						result.push({
+							key:
+								planosContaAux[i].descricao +
+								' (' +
+								planosContaAux[i].tipoMovimento +
+								')',
+						});
+					}
+
+					setListData(result);
+					setPlanosConta(res.data);
+				})
+				.catch((err) => {
+					Toast.show({
+						type: 'error',
+						position: 'top',
+						text1: err.message,
+					});
+				});
 		}
 
-		load()
-		
-		
-	},[])
+		load();
+	}, []);
 
 	return (
-<<<<<<< HEAD
-		<View>
-			{/* <Header name='Usuário ' onPress={() => console.log('abrir')} /> */}
-			<View>
-				<Card>
-					<Text>Planos de Contas</Text>
-					<TextInput
-=======
 		<Center>
 			<Card>
 				<Text>Plano de Contas</Text>
-					<TextInput		
->>>>>>> bc62128defe60e8a9f56f03cd55c161c99dc4bdf
-						placeholderTextColor='#878686'
-						placeholder='Nome'
-						textContentType='name'
-						blurOnSubmit={false}
-						style={[styles.input, { marginBottom: 40 }]}
-						value={descricao}
-						onChangeText={(text:string)=>handleChange(setDescricao, "descricao", text)}
-					></TextInput>
-					<Picker selectedValue={tipoMovimento} onValueChange={(value) => handleChange(setTipoMovimento, "tipoMovimento", value.toString())}>
-						{planosConta.map(planoConta =>{
-							if(planoConta.padrao){
-								return(
-									<Picker.Item label={planoConta.descricao+" ("+planoConta.tipoMovimento+") "} value={planoConta.tipoMovimento}/>	
-								)
-							}
-							
-						})}
-						
-					</Picker>
-					<Button title="Adicionar" onPress={adicionarPlanoContas}/>
-				<FlatList data={listData}  renderItem={({item}) => <Text>{item.key} </Text>}/>
+				<TextInput
+					placeholderTextColor='#878686'
+					placeholder='Nome'
+					textContentType='name'
+					blurOnSubmit={false}
+					style={[styles.input, { marginBottom: 40 }]}
+					value={descricao}
+					onChangeText={(text: string) =>
+						handleChange(setDescricao, 'descricao', text)
+					}
+				></TextInput>
+				<Picker
+					selectedValue={tipoMovimento}
+					onValueChange={(value) =>
+						handleChange(setTipoMovimento, 'tipoMovimento', value.toString())
+					}
+				>
+					{globalPlanosConta.map((planoConta) => {
+						if (planoConta.padrao) {
+							return (
+								<Picker.Item
+									label={
+										planoConta.descricao +
+										' (' +
+										planoConta.tipoMovimento +
+										') '
+									}
+									value={planoConta.tipoMovimento}
+								/>
+							);
+						}
+					})}
+				</Picker>
+				<Button title='Adicionar' onPress={adicionarPlanoContas} />
+				<FlatList
+					data={listData}
+					renderItem={({ item }) => <Text>{item.key} </Text>}
+				/>
 			</Card>
 		</Center>
 	);
--- src/store/modules/user/action.ts
+++ src/store/modules/user/action.ts
@@ -1,10 +1,28 @@
-import { IUser } from "../../../types";
+import { IPlanoconta, IUser } from '../../../types';
 
 export function LoginUser(user: IUser) {
 	return {
-	  type: 'LOGIN',
-	  payload: {
-		user
-	  }
-	}
-  }
+		type: 'LOGIN',
+		payload: {
+			user,
+		},
+	};
+}
+
+export function savePlanosConta(planosConta: IPlanoconta[]) {
+	return {
+		type: 'ADD_PLANOS_CONTA',
+		payload: {
+			planosConta,
+		},
+	};
+}
+
+export function putPlanoConta(planoConta: IPlanoconta) {
+	return {
+		type: 'PUT_PLANO_CONTA',
+		payload: {
+			planoConta,
+		},
+	};
+}
